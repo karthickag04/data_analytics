0c111f4473a6974f88461040dfbd0046ea4462c0
karthickag04 <karthickag04@gmail.com>
2024-08-03 14:11:48 +0530

    added session 1,2,3

---
+++ batch_01/python/demo.py
@@ -0,0 +1,6 @@
+def addition():
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    print("The sum of the two numbers is: ", num1 + num2)
+
+addition()
